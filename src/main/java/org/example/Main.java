5f219f2504eded4ff314a88d1bae6baf83b2aaf2
Carlostorres8791 <escolta87@hotmail.com>
2023-06-28 00:06:58 -0500

    ejericio1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // Imprimir los datos vistos en las sesiones
+        int numeroEntero = 10;
+        System.out.println("Número entero: " + numeroEntero);
+
+        long numeroLargo = 1000000000L;
+        System.out.println("Número largo: " + numeroLargo);
+
+        double numeroDecimal = 3.14;
+        System.out.println("Número decimal: " + numeroDecimal);
+
+        boolean esVerdadero = true;
+        System.out.println("Booleano: " + esVerdadero);
+
+        String texto = "Hola, mundo!";
+        System.out.println("Texto: " + texto);
+    }
+}
